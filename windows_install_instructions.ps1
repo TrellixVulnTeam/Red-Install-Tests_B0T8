80591dc6e7bcc557c71862338e427c398e6c5e9a
jack1142 <6032823+jack1142@users.noreply.github.com>
2022-07-16 20:24:26 +0200

    Fix exit code for PowerShell snippet *again*
    Test `-OutputFormat Text`

---
+++ windows_install_instructions.ps1
@@ -0,0 +1,34 @@
+# `ps:` doesn't include $ErrorActionPreference so gotta add it manually
+# Also, that only works for cmdlets so we have to check exit code FOR EACH COMMAND.
+# PowerShell 7.3 will fix this but not like we're going to have that in CI...
+# https://github.com/PowerShell/PowerShell-RFC/pull/277
+$ErrorActionPreference = "Stop"
+$ProgressPreference = "SilentlyContinue"
+
+$commands = @"
+Set-ExecutionPolicy Bypass -Scope Process -Force
+[System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072
+iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+# v-- stop log spam by disabling download progress (not part of instructions)
+choco feature disable -n=showDownloadProgress
+# ^-- stop log spam by disabling download progress (not part of instructions)
+choco upgrade git --params "/GitOnlyOnPath /WindowsTerminal" -y
+choco upgrade visualstudio2022-workload-vctools -y
+choco upgrade python3 -y --version 3.9.13
+
+choco upgrade temurin11 -y --version 11.0.14.10100
+"@
+
+ForEach ($command in $($commands -split '\r?\n'))
+{
+    Write-Host "$(prompt)$command"
+    if (!$command -or $command.StartsWith('#'))
+    {
+        continue
+    }
+    Invoke-Expression "$command; `$output = `$?"
+    if (!$output)
+    {
+        exit 1
+    }
+}
